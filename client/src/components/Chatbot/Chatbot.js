9d95d90fffb59e91606e34bfc78c4cd39b640b93
Huynh Cong Minh <huynhcongminh@Huynhs-MacBook-Pro.local>
2019-10-02 17:52:04 +1000

    commit

--- client/src/components/Chatbot/Chatbot.js
+++ client/src/components/Chatbot/Chatbot.js
@@ -17,9 +17,9 @@ export default class Chatbot extends Component {
           <h1 className="display-4">Chatbot Support</h1>
           <p className="lead">
             <ChatBot
-              title="My React Bot"
-              botName="BookTripBotMOBILEHUB"
-              welcomeMessage="Welcome, how can I help you today?"
+              title="Muscle Bot"
+              botName="EzMuscles"
+              welcomeMessage="Welcome to Ez-Muscles Largest Supplement Retail Store"
               onComplete={this.handleComplete.bind(this)}
               clearOnComplete={true}
             />
