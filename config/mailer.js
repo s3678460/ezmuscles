180be882ea9bc190d46c4197aed8203439c26765
Huynh Cong Minh <huynhcongminh@Huynhs-MacBook-Pro.local>
2019-10-15 15:52:12 +1100

    minh commit

--- config/mailer.js
+++ config/mailer.js
@@ -1,5 +1,5 @@
 module.exports={
-    MAILGUN_USER:'postmaster@sandboxe40fc95e7eec40a284d54b62353988f2.mailgun.org',
-    MAILGUN_PASSWORD:'90cce097b65d3be29a120ba8dfead13d-af6c0cec-3097a9e7',
+    MAILGUN_USER:'postmaster@reply.theezmuscle.com',
+    MAILGUN_PASSWORD:'62269214f7a692d2bbc12d5827ca88cf-9c988ee3-f6fb94fc',
 
 }
