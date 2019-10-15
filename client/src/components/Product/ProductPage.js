836afbc895a0a7a84c23b3ea3d50592a5a393457
Huynh Cong Minh <huynhcongminh@Huynhs-MacBook-Pro.local>
2019-10-15 15:04:41 +1100

    minh commit

--- client/src/components/Product/ProductPage.js
+++ client/src/components/Product/ProductPage.js
@@ -4,46 +4,56 @@ import { getProducts } from "../../actions/productActions";
 import { Link } from "react-router-dom";
 
 export class ProductPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            searchByKey: ""
-        }
-    }
-    componentDidMount() {
-        this.props.getProducts()
-    }
-    onChange = (e) => {
-        var target = e.target;
-        var name = target.name;
-        var value = target.value;
-        this.setState({
-            [name]: value
-        })
-    }
-    closeSearch = (e) => {
-        this.setState({ searchByKey: "" })
-    }
-    render() {
-        var { products } = this.props.products
-        //check search by key
-        products = products.filter((product) => {
-            return product.name.toLowerCase().indexOf(this.state.searchByKey.toLowerCase()) !== -1
-        })
-        //render list products
-        var listProducts = products.map((product, index) => {
-            return <div key={index} className="row pt-4">
-                <div className="col-12 hoverable" style={{ backgroundColor: "white" }}>
-                    <div className="row">
-                        <div className="col-6">
-                            <div style={{ padding: "12px 12px 12px 0px" }}>
-                                <img style={{ maxHeight: "100%", maxWidth: "100%" }}
-                                    src={`https://ezmuscles.s3-ap-southeast-2.amazonaws.com/${product.imageURL}`}
-                                    className="img-fluid" />
-                            </div>
-                        </div>
-                        <div className="col-6">
-                            <div style={{ paddingTop: "12px" }}>
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchByKey: ""
+    };
+  }
+  componentDidMount() {
+    this.props.getProducts();
+  }
+  onChange = e => {
+    var target = e.target;
+    var name = target.name;
+    var value = target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+  closeSearch = e => {
+    this.setState({ searchByKey: "" });
+  };
+  render() {
+    var { products } = this.props.products;
+    //check search by key
+    products = products.filter(product => {
+      return (
+        product.name
+          .toLowerCase()
+          .indexOf(this.state.searchByKey.toLowerCase()) !== -1
+      );
+    });
+    //render list products
+    var listProducts = products.map((product, index) => {
+      return (
+        <div key={index} className="row pt-4">
+          <div
+            className="col-12 hoverable"
+            style={{ backgroundColor: "white" }}
+          >
+            <div className="row">
+              <div className="col-6">
+                <div style={{ padding: "12px 12px 12px 0px" }}>
+                  <img
+                    style={{ maxHeight: "100%", maxWidth: "100%" }}
+                    src={`https://ezmuscles.s3-ap-southeast-2.amazonaws.com/${product.imageURL}`}
+                    className="img-fluid"
+                  />
+                </div>
+              </div>
+              <div className="col-6">
+                {/* <div style={{ paddingTop: "12px" }}>
                                 <h2 className="h2-responsive">{product.name}</h2>
                                 <dl className="row">
                                     <dt className="col-sm-3">Price</dt>
@@ -59,53 +69,72 @@ export class ProductPage extends Component {
                                     <div className="col-sm-12">
                                         <Link
                                             className="btn btn-success"
-                                        // to={`/buy/${product._id}`}
-                                        >Buy</Link>
+                                         to={`/purchase/${product._id}`}
+                                        >Purchase</Link>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
+                            </div> */}
+                <div className="card" style={{ width: "25rem" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {product.category}
+                    </h6>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {product.price}
+                    </h6>
+                    <p className="card-text">{product.description}</p>
+                    <a className="card-link">
+                      <Link
+                        className="btn btn-success"
+                        to={`/purchase/${product._id}`}
+                      >
+                        Purchase
+                      </Link>
+                    </a>
                   </div>
                 </div>
-             
-        })
-        return (
-            <div className="pt-5">
-                <div className="container">
-                    <div>
-                        <button
-                            onClick={this.closeSearch}
-                            className="btn btn-primary"
-                            type="button" data-toggle="collapse"
-                            data-target="#collapseExample"
-                            aria-expanded="false"
-                            aria-controls="collapseExample">
-                            Search
-                    </button>
-                    </div>
-                    <div className="collapse" id="collapseExample">
-                        <div className="card card-body">
-                            <input
-                                value={this.state.searchByKey}
-                                name="searchByKey"
-                                onChange={this.onChange}
-                                className="form-control mr-sm-2"
-                                type="search"
-                                placeholder="Search by name"
-                                aria-label="Search"
-                            />
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    {listProducts}
-                </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      );
+    });
+    return (
+      <div className="pt-5">
+        <div className="container">
+          <div>
+            <button
+              onClick={this.closeSearch}
+              className="btn btn-primary"
+              type="button"
+              data-toggle="collapse"
+              data-target="#collapseExample"
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              Search
+            </button>
+          </div>
+          <div className="collapse" id="collapseExample">
+            <div className="card card-body">
+              <input
+                value={this.state.searchByKey}
+                name="searchByKey"
+                onChange={this.onChange}
+                className="form-control mr-sm-2"
+                type="search"
+                placeholder="Search by name"
+                aria-label="Search"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container">{listProducts}</div>
+      </div>
+    );
+  }
 }
-
 
 const mapStateToProps = state => {
   return {
